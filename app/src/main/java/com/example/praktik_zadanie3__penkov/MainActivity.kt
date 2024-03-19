1c98d6653d8a174e4cf0b9df73f86dbb0263e25f
Student <kolasiskin213@gmail.com>
2024-03-19 09:59:58 +0300

    Задание 3 Макет Шишкин

---
+++ app/src/main/java/com/example/praktik_zadanie3__penkov/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.praktik_zadanie3__penkov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
