d98688dcd7932b85f4241cb391ff8ea783c38b13
Student <kolasiskin213@gmail.com>
2024-03-19 10:45:13 +0300

    Зад 3

--- app/src/main/java/com/example/praktik_zadanie3__penkov/MainActivity.kt
+++ app/src/main/java/com/example/praktik_zadanie3__penkov/MainActivity.kt
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
-
+import com.example.praktik_zadanie3__penkov.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }}
 
-    }
-
-}
